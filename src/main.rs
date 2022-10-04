0a4e7336f45cd58e91abc3f6f2c96332ab74f9b4
Mattia Valzelli <mattia@mvalzelli.com>
2022-10-04 22:52:44 +0200

    Chapter 3: finish work

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
+use sqlx::PgPool;
 use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let address = TcpListener::bind("127.0.0.1:8000")?;
-    run(address)?.await
+    // Panic if we can't read configuration
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection_pool)?.await
 }
