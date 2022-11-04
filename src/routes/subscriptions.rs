85cc9f1e0d45a2d1f6f9c4fc945068de80488c70
Mattia Valzelli <mattia@mvalzelli.com>
2022-11-04 18:01:55 +0100

    Run format all

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -20,9 +20,17 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     // FIXME: We need to remove the `enter` here because it does not
     // play well with async functions
     let _request_span_guard = request_span.enter();
-    
-    tracing::info!("request_id: {} - Adding '{}' '{}' as a new subscriber", request_id, form.email, form.name);
-    tracing::info!("request_id: {} - Saving new subscriber details in the database", request_id);
+
+    tracing::info!(
+        "request_id: {} - Adding '{}' '{}' as a new subscriber",
+        request_id,
+        form.email,
+        form.name
+    );
+    tracing::info!(
+        "request_id: {} - Saving new subscriber details in the database",
+        request_id
+    );
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -37,11 +45,18 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     .await
     {
         Ok(_) => {
-            tracing::info!("request_id: {} - New subscriber details have been saved", request_id);
+            tracing::info!(
+                "request_id: {} - New subscriber details have been saved",
+                request_id
+            );
             HttpResponse::Ok().finish()
-        },
+        }
         Err(e) => {
-            tracing::error!("request_id: {} - Failed to execute query: {:?}", request_id, e);
+            tracing::error!(
+                "request_id: {} - Failed to execute query: {:?}",
+                request_id,
+                e
+            );
             HttpResponse::InternalServerError().finish()
         }
     }
