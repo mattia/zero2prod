6c06a2f5a9e6058ebd6dbc723e22480bf849bb7c
Mattia Valzelli <me@mattia.cloud>
2024-07-15 17:34:24 +0200

    First web framework with integration test
    Signed-off-by: Mattia Valzelli <me@mattia.cloud>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
-fn main() {
-    println!("Hello, world!");
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    run()?.await
 }
---
+++ tests/health_check.rs
@@ -0,0 +1,28 @@
+//! tests/health_check.rs
+
+#[tokio::test]
+async fn dummy_test() {
+    // Arrange
+    spawn_app();
+
+    let client = reqwest::Client::new();
+
+    // Act
+    let response = client
+        .get("http://127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+fn spawn_app() {
+    let server = zero2prod::run().expect("Failed to bind address");
+    // Launch the server as a background task
+    // tokio::spawn returns a handle to the spawned future,
+    // but we have no use for it here, hence the non-binding let
+    let _ = tokio::spawn(server);
+}
